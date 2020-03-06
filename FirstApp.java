96a02e150d5e59a7858563a36b940dbda6e31296
yuvarajmms <31970667+yuvarajmms@users.noreply.github.com>
2020-03-06 12:08:16 +0530

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -1,6 +1,6 @@
 public class FirstApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World from java!");
         System.out.println(" - java.version: " + System.getProperty("java.version"));
         System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
     }
